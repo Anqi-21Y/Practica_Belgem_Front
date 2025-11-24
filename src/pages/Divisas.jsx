d45fd0ab03d06860fe7c51dd6aba15084b2127a2
jhoa.mar18 <jhoa.mar18@alumnes.ilerna.com>
2025-11-24 05:26:49 +0100

    limpiar comentarios y dejar divisas listo

--- src/pages/Divisas.jsx
+++ src/pages/Divisas.jsx
@@ -1,58 +1,49 @@
 import { Link } from "react-router-dom";
 export default function Divisas() {
-    const divisas =[
+  
+  const divisas =[
         {id: 1, codigo: "USD", cotizacion: 2.00, fechaActualizacion: "2025-11-20" },
         {id: 2, codigo: "EUR", cotizacion: 4.50, fechaActualizacion: "2025-11-19" },
         {id: 3, codigo: "GBP", cotizacion: 6.95, fechaActualizacion: "2025-11-18" },
-    ]
+  ];
+  
   return (
+    <section id="divisas">
+      <h1>Lista de  Divisas</h1>
+      <p> esto es un parrafo de divisas</p>
     
-   
-  <section id="divisas">
-    <h1>Lista de  Divisas</h1>
-    <p> esto es un parrafo de divisas</p>
-    
-    {/* aqui debe ir la tabla de divisas */}
-    {/* aqui los encabezados de la tabla */}
-    {/* y aqui deben de mostrarse las filas repetidas de cada divisa */}
-        
-    <table>
-  <thead>
-    <tr>
-      <th>Código</th>
-      <th>Cotización</th>
-      <th>Fecha actualización</th>
-      <th>Acciones</th>
-    </tr>
-  </thead>
+      <table>
+        <thead>
+          <tr>
+            <th>Código</th>
+            <th>Cotización</th>
+            <th>Fecha actualización</th>
+            <th>Acciones</th>
+          </tr>
+        </thead>
 
-  <tbody>
-    {/* aqui van las filas */}
-    {divisas.map((divisa) => (
-  <tr key={divisa.id}>
-    <td>{divisa.codigo}</td>
-    <td>{divisa.cotizacion}</td>
-    <td>{divisa.fechaActualizacion}</td>
-    <td>
-        <Link to={`/divisas/editar/${divisa.id}`}>
-            <button>Editar</button>
-        </Link>
+        <tbody>
+          {divisas.map((divisa) => (
+            <tr key={divisa.id}>
+              <td>{divisa.codigo}</td>
+              <td>{divisa.cotizacion}</td>
+              <td>{divisa.fechaActualizacion}</td>
+              <td>
+                <Link to={`/divisas/editar/${divisa.id}`}>
+                  <button>Editar</button>
+                </Link>
 
-        <button>Borrar</button>
+                <button>Borrar</button>
 
-        <Link to="/divisas/nueva">
-            <button>Nueva Divisa</button>
-        </Link>
-
-    </td>
-  </tr>
-))}
-
-  </tbody>
-</table>
-   
-   
-  </section>
+                <Link to="/divisas/nueva">
+                  <button>Nueva Divisa</button>
+                </Link>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </section>
   );
 }
 
